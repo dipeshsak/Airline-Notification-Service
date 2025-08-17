6e541891415fb75472860c5a3c76e0330532994a
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2025-08-17 15:32:15 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const express = require('express');
 
+const amqplib  = require("amqplib");
+const { EmailService } = require('./services')
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            // const object = JSON.parse(Buffer.from(data).toString());
+            await EmailService.sendEmail("amazinginventos@gmail.com", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch(error) {
+        
+    }
+}
+
+
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 // const mailsender = require('./config/email-config')
@@ -24,4 +45,6 @@ app.listen(ServerConfig.PORT, async() => {
     // } catch(error) {
     //     console.log(error);
     // }
+    await connectQueue();
+    console.log("queue is up")
 });
