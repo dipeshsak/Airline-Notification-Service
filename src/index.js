7391e005dac12b33edc44771537aa3c1692637d9
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2025-08-17 15:22:27 +0530

    added create ticket api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const mailsender = require('./config/email-config')
+// const mailsender = require('./config/email-config')
 
 const app = express();
 
@@ -13,15 +13,15 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-     try {
-        const response = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'dipeshsakharkar1@gmail.com',
-            subject: 'Is the service working ? now as well',
-            text: 'Yes it is working'
-        });
-        console.log(response);
-    } catch(error) {
-        console.log(error);
-    }
+    //  try {
+    //     const response = await mailsender.sendMail({
+    //         from: ServerConfig.GMAIL_EMAIL,
+    //         to: 'dipeshsakharkar@gmail.com',
+    //         subject: 'Is the service working ? now as well',
+    //         text: 'Yes it is working'
+    //     });
+    //     console.log(response);
+    // } catch(error) {
+    //     console.log(error);
+    // }
 });
